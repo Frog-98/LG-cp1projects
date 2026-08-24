25a71b95140f50374005d1eb76369ce239e03c78
Frog-98 <lincoln.girot@ucas-edu.net>
2026-08-24 09:25:39 -0600

    Fixed the debugging project

--- Debugging.py
+++ Debugging.py
@@ -1,16 +1,9 @@
 #Lincoln Girot, Debugging
 
-#Broken Code
-name = "Alex"
-age = 16 #No quotatons and variables cannot start with a number.
-
-print("My name is " + nam + " and I am " + str(age) + " years old."#No parentheses
-                      #Typo
-
-      
 #Fixed Code
 
 name = "Alex"
-age = "Sixteen"
+age = "Sixteen" #This use to have no quotation marks and started with a number.
 
-print("My name is " + name + " and I am " + str(age) + " years old.")
+print("My name is " + name + " and I am " + str(age) + " years old.")#There use to be no parentheses here.
+                      #This use to say "nam" and not "name"
