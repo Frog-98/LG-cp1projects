0c52f1ab9f5c8933c9ece4dc1c025f46efa363c6
Frog-98 <lincoln.girot@ucas-edu.net>
2026-08-27 09:40:11 -0600

    Did projects

---
+++ Interactive_Introduction.py
@@ -0,0 +1,8 @@
+#Lincoln Girot, Interactive Introduction
+
+name = input("What is your name?")
+age = input ("How old are you?")
+home = input("Where do you live?")
+color = input("What is your favorite color?")
+
+print(name + " Is" + age + " Years old, Lives in" + home + " and their favorite color is" + color)
---
+++ Interview.py
@@ -0,0 +1,17 @@
+#Lincoln Girot, Interview
+
+name = print("What is your name?")
+age = print("How old are you?")
+home = print("Where do you live?")
+color = print("What is your favorite color?")
+car = print("What is your favorite car?")
+animal = print("What is your favorite animal?")
+drink = print("What is your favorite drink?")
+candy = print("What is your favorite candy?")
+food = print("What is your favorite  food?")
+video_game = print("What is your favorite video game?")
+sport = print("What is your favorite sport?")
+favorite_place = print("What is your favorite place?")
+
+print(name + "is" + age + "years old, lives in" + home + "and their favorite color is" + color + ". Their favorite car is the" + car + ". Their favorite animal is the" + animal + ". They like drinking" + drink + "and eating" + candy + "and" + food + "They like playing" + video_game + "and" + sport + "and their favorite place is" + favorite_place )
+
