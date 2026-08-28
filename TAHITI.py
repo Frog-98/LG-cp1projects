2c3f79d7199697c7d39531fd902e3f337d40e849
Frog-98 <lincoln.girot@ucas-edu.net>
2026-08-28 09:52:55 -0600

    Changes

---
+++ TAHITI.py
@@ -0,0 +1,9 @@
+#Lincoln Girot, TAHITI
+
+import random
+
+quotes = ["MY NAME IS JOHN MARSTON!", "I love my horsey and my horsey love me.", "WE NEED MONEY.", "Have some faith, Arthur.", "LENNY!", "Just one more score."]
+
+random_quote = random.choice(quotes)
+
+print(random_quote)
