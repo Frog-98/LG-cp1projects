ddf7972aa5bc058c3dfd017cc23e328309c22471
frog-98 <Lincoln.girot@ucas-edu.net>
2026-08-31 09:52:36 -0600

    notes

---
+++ Integers_floats.py
@@ -0,0 +1,22 @@
+#Integers and Floats notes, LG
+
+#Integer - A whole number
+num = 75 # <= Just write the number no extra syntax
+
+# Float # <= Just a number with a decimal point
+pi = 3.1415 # <= Just write the number no extra syntax
+
+# Arithmetic Operators ( + - * / ** // %)
+print(5//2) # <= Integer division
+print(5/2) # <= Will always output a float
+
+# Modulo = % (modulus)
+print(5%2)
+print(10%4)
+print(15%5)
+
+print((2-1)*3+4%3)
+# Order of Operations (PEMDAS)
+
+# assignment operator =
+
--- TAHITI.py
+++ TAHITI.py
@@ -2,7 +2,7 @@
 
 import random
 
-quotes = ["MY NAME IS JOHN MARSTON!", "I love my horsey and my horsey love me.", "WE NEED MONEY.", "Have some faith, Arthur.", "LENNY!", "Just one more score."]
+quotes = ["MY NAME IS JOHN MARSTON!", "I love my horsey and my horsey love me.", "WE NEED MONEY.", "Have some faith, Arthur.", "LENNY!", "Just one more score.", "You're a good man Arthur Morgan.", "ITS JOHN MARSTON MICAH!"]
 
 random_quote = random.choice(quotes)
 
