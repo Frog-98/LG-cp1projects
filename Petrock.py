d52ee1764f373e7aaf3bec19ad81ae96cec69f04
frog-98 <Lincoln.girot@ucas-edu.net>
2026-09-10 09:53:04 -0600

    pet rock

---
+++ Petrock.py
@@ -0,0 +1,18 @@
+#Lincoln Girot, Pet Rock
+
+print('Welcome to pet rock! Type "feed" to feed your pet rock! Type "pet" to pet your pet rock!')
+
+
+user_input = input("")
+
+
+if user_input == "feed":
+    print("Pet Rock Has Been Fed!")
+elif user_input == "pet":
+    print("Pet rock has been pet!")
+else:
+    print("Your pet rock has died.")
+
+
+
+
