5b7601625907e9ba046104aa3f7590a647a6e065
frog-98 <Lincoln.girot@ucas-edu.net>
2026-09-11 09:15:36 -0600

    Madlib

--- Madlib.py
+++ Madlib.py
@@ -6,6 +6,6 @@ noun2 = input("Choose a noun.")
 noun3 = input("Choose a place or building.")
 adjective2 = input("Choose an adjective.")
 
-madlib = ("Last week I went on a very " + (adjective1) + " vaction to " + (noun1) + ". While we were there we saw a giant" + (noun2) + " and stayed inside of a" + (noun3) + ". Overall I really liked the vacation and though it was very" + (adjective2))
+madlib = ("Last week I went on a very " + (adjective1) + " and" + (adjective2) + " vaction to " + (noun1) + ". While we were there we saw a giant" + (noun2) + " and stayed inside of a" + (noun3) + ". Overall I really liked the vacation and thought it was very" + (adjective1) + " and" + (adjective2))
 
 print(madlib)
---
+++ notes.py
@@ -0,0 +1,4 @@
+#Lincoln Girot, Notes
+
+#Pseudocode is a set of instructions that show show to create a code\
+
