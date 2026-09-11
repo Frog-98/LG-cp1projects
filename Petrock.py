3c226128690a4d5a6110e6dadf15dbcb76cc1c39
frog-98 <Lincoln.girot@ucas-edu.net>
2026-09-11 09:48:24 -0600

    Pet Rock

--- Petrock.py
+++ Petrock.py
@@ -1,18 +1,57 @@
 #Lincoln Girot, Pet Rock
 
-print('Welcome to pet rock! Type "feed" to feed your pet rock! Type "pet" to pet your pet rock!')
+import random
+import time
+
+print('Welcome to pet rock! Type "feed" to feed your pet rock! Type "pet" to pet your pet rock, and type "wash" yuro wash your pet rock!')
+
+start_time = time.time()
+last_fed_time = time.time()
+last_pet_time = time.time()
+last_wash_time = time.time()
+
+while True:
+    current_time = time.time()
+
+    if current_time - last_fed_time > 15:
+        print("\n Oh no! Your pet rock has starved.")
+        break
+
+    if current_time - last_pet_time > 30:
+        print("\n Oh no! Your pet rock died of depression from not being pet.")
+        break
+
+    if current_time - last_wash_time > 60:
+        print("\n Oh no! Your pet rock has died of sickness from not being washed.")
+        break
+
+    if random.random() < 0.005:
+        print("\n Oh no! A seagull stole your pet rock!")
+        break
+
+    if random.random() < 0.001:
+        print("\n Oh no! Your pet rock has died of cancer!")
+        break
+
+    user_input = input("\nWhat would you like to do? ").lower().strip()
 
 
-user_input = input("")
+    if user_input == "feed":
+        print("Pet Rock Has Been Fed!")
+        last_fed_time = time.time()
+    elif user_input == "pet":
+        print("Pet rock has been pet!")
+        last_pet_time = time.time()
+    elif user_input == "wash":
+        print("You pet rock has been washed!")
+        last_wash_time = time.time()
+    else:
+        print("Your pet rock has died.")
+        break
 
+end_time = time.time()
+total_time_alive = round(end_time - start_time, 1)
 
-if user_input == "feed":
-    print("Pet Rock Has Been Fed!")
-elif user_input == "pet":
-    print("Pet rock has been pet!")
-else:
-    print("Your pet rock has died.")
-
-
+print(f"\n Your pet rock has died. It survived for {total_time_alive} seconds.")
 
 
