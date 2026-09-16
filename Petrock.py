3d77317f2bb3e3f09eb010f92c8bc5cf8e610bf7
frog-98 <Lincoln.girot@ucas-edu.net>
2026-09-16 09:38:59 -0600

    pet rock

--- Petrock.py
+++ Petrock.py
@@ -3,35 +3,54 @@
 import random
 import time
 
-print('Welcome to pet rock! Type "feed" to feed your pet rock! Type "pet" to pet your pet rock, and type "wash" yuro wash your pet rock!')
+print('Welcome to pet rock! Type "feed" to feed your pet rock! Type "pet" to pet your pet rock, type "wash" to wash your pet rock and type "sunscreen" to put suncreen on your pet rock! ')
 
 start_time = time.time()
 last_fed_time = time.time()
 last_pet_time = time.time()
 last_wash_time = time.time()
+last_sunscreen_time = time.time()
 
 while True:
     current_time = time.time()
 
-    if current_time - last_fed_time > 15:
+    if current_time - last_fed_time > 10:
         print("\n Oh no! Your pet rock has starved.")
         break
 
-    if current_time - last_pet_time > 30:
+    if current_time - last_pet_time > 15:
         print("\n Oh no! Your pet rock died of depression from not being pet.")
         break
 
-    if current_time - last_wash_time > 60:
+    if current_time - last_wash_time > 20:
         print("\n Oh no! Your pet rock has died of sickness from not being washed.")
         break
 
-    if random.random() < 0.007:
+    if current_time - last_sunscreen_time > 30:
+            print("\n Oh no! Your pet rock has died of sunburn!")
+            break
+
+    if random.random() < 0.01:
         print("\n Oh no! A seagull stole your pet rock!")
         break
 
-    if random.random() < 0.001:
+    if random.random() < 0.005:
         print("\n Oh no! Your pet rock has died of cancer!")
         break
+
+    if random.random() < 0.001:
+        print("\n Oh no! Your pet rock spontaneously combusted!")
+        break
+
+    if random.random() < 0.001:
+            print("\n Oh no! Your pet rock was drafted into World War 3!")
+            break
+
+    if random.random() < 0.0005:
+                print("\n Oh no! Your pet rock was killed in a firery 4 car car crash on the freeway after attempting to flee in a high speed chase after recieving a $30 parking ticket!")
+                break
+    
+
 
     user_input = input("\nWhat would you like to do? ").lower().strip()
 
@@ -45,6 +64,9 @@ while True:
     elif user_input == "wash":
         print("You pet rock has been washed!")
         last_wash_time = time.time()
+    elif user_input == "sunscreen":
+        print("Your pet rock has been given sunscreen!")
+        last_sunscreen_time = time.time()
     else:
         print("Your pet rock has died.")
         break
